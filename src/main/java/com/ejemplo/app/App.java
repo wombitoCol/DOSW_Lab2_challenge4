79f0b17a138569a51492d06988449a200789db4d
wombitoCol <julio.mayorquin@mail.escuelaing.edu.co>
2026-02-07 15:04:18 -0500

    maven config

---
+++ src/main/java/com/ejemplo/app/App.java
@@ -0,0 +1,13 @@
+package com.ejemplo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
